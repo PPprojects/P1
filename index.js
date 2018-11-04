314d768260576a220aeaec804a392e7f993af5df
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-11-03 20:20:50 -0400

    slightly imporved but still only operational thanks to setTimeOut

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const app = require('./server')
 const PORT = process.env.PORT || 1337
 const { db } = require('./server/db/models')
-const chalk = require('chalk')
 const chalkAnimation = require('chalk-animation')
 
 db.sync({ force: false })
 	.then(() => {
-		const message = chalkAnimation.rainbow(
-			'Postgres server is up and running...'
+		const message = chalkAnimation.pulse(
+			'Postgres server is up and running...',
+			1.25
 		)
 		app.listen(PORT, (err) => {
 			if (err) throw err
 			setTimeout(() => {
 				const message2 = chalkAnimation.neon(`Listening on port ${PORT}.`)
-			}, 4000)
+			}, 3000)
 		})
 	})
 	.catch(console.error)
